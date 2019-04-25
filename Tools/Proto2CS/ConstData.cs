7cad281ae3b321c6472d5d826df027f680ca8f28
fanrong <FRee22858@live.com>
2019-04-26 00:22:19 +0800

    添加生成协议

---
+++ Tools/Proto2CS/ConstData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proto2CS
+{
+    public class ConstData
+    {
+        public const string PROTOC_PATH = "../../ThirdParty/google_protoc/";
+        public const string PROTO_PATH = "../../Protocol/";
+    }
+}
---
+++ Tools/Proto2CS/Program.cs
@@ -0,0 +1,27 @@
+﻿using Model.Helper;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Proto2CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string protoc = "";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                protoc = "protoc.exe";
+            }
+            else
+            {
+                protoc = "protoc";
+            }
+            protoc = ConstData.PROTOC_PATH + protoc;
+            ProcessHelper.Run(protoc, "--csharp_out=\"../../Message/\" --proto_path=\""+ConstData.PROTO_PATH+"\" OuterMessage.proto", waitExit: true);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
