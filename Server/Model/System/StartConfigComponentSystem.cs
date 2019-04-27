6a8d4885ca2e645142166b19cbe585269df64740
Boil <FRee22858@live.com>
2019-04-27 17:48:49 +0800

    feature [配置文件] 配置文件组件框架

---
+++ Server/Model/System/StartConfigComponentSystem.cs
@@ -0,0 +1,15 @@
+﻿using Model.Base.RDAttribute;
+using Model.Base.System;
+using Model.Component.Config;
+
+namespace Model.System
+{
+    [ObjectSystem]
+    public class StartConfigComponentSystem : AAwakeSystem<StartConfigComponent, string, int>
+    {
+        public override void Awake(StartConfigComponent self, string a, int b)
+        {
+            self.Awake(a, b);
+        }
+    }
+}
